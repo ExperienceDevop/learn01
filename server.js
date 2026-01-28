64af9399d59b168de7c5060279329100f99d428d
ExperienceDevop <109736678+ExperienceDevop@users.noreply.github.com>
2026-01-28 20:05:57 +0000

    modified:   server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require ("express")
-const errorHandler = require("./middleware/errorHandler")
+//const errorHandler = require("./middleware/errorHandler")
 const dotenv = require ("dotenv").config ()
 
 const app = express ()
